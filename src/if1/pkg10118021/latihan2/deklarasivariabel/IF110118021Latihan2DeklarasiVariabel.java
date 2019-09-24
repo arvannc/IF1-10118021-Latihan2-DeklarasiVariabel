78faebb310e34c0a28d8d5c19e568648aeeea36b
M Arvan Nurcahyana <55648161+arvannc@users.noreply.github.com>
2019-09-24 18:24:47 +0700

    Add files via upload

---
+++ src/if1/pkg10118021/latihan2/deklarasivariabel/IF110118021Latihan2DeklarasiVariabel.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118021.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author Arvan
+ * NAMA     : M Arvan Nurcahyana
+ * KELAS    : IF-1
+ * NIM      : 10118021
+ * Deskripsi Program :Deklarasi dan Pengaksesan Program
+ */
+public class IF110118021Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi Variabel nilai = " + nilaiInt);
+        System.out.println("Isi Variabel PHI = " + PHI);
+        System.out.println("Isi Variable Logika = " + nilaiLogika);
+        System.out.println("Isi Variabel Karakter = " + nilaiKarakter);
+        
+    }
+    
+}
